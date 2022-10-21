80244b8a0af199518ffcfbc5c3c9ea72f74ce3d4
Thiago Costa <41833533+thi-costa@users.noreply.github.com>
2022-10-21 19:24:26 -0300

    Initial version

---
+++ src/ConversorDeTemperatura.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ConversorDeTemperatura {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.printf("-=-=-=-=-=-=-=-=-=-=-=-=-=\nCONVERSOR DE TEMPERATURAS\n-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.printf("\nQuantas temperaturas você deseja CONVERTER: ");
+        int quantidadeDeTemperaturas = entrada.nextInt();
+
+        for(int iteracao=0; iteracao<quantidadeDeTemperaturas; iteracao++){
+
+        }
+    }
+
+}
